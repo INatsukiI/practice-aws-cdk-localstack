c9d954d03a54150373d9e47e5c7755e405be4a70
INatsukiI <e1q20019@oit.ac.jp>
2025-02-18 00:08:20 +0900

    add: s3・lambda・sns構成

--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -1,6 +1,10 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as s3n from 'aws-cdk-lib/aws-s3-notifications';
+import * as sns from 'aws-cdk-lib/aws-sns';
+import * as subscriptions from 'aws-cdk-lib/aws-sns-subscriptions';
+import { Construct } from 'constructs';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -10,5 +14,27 @@ export class CdkStack extends cdk.Stack {
       bucketName: 'my-bucket-name',
       versioned: true,
     })
+
+    const topic = new sns.Topic(this, 'NotificationTopic', {
+      displayName: 'S3 Upload Notifications'
+    });
+
+    topic.addSubscription(new subscriptions.EmailSubscription(process.env.EMAIL_SUBSCRIPTION as string));
+
+    const handler = new lambda.Function(this, 'NotificationHandler', {
+      runtime: lambda.Runtime.PYTHON_3_12,
+      handler: 'notification.handler',
+      code: lambda.Code.fromAsset('lambda'),
+      environment: {
+        TOPIC_ARN: topic.topicArn
+      }
+    });
+
+    topic.grantPublish(handler);
+    
+    bucket.addEventNotification(
+      s3.EventType.OBJECT_CREATED_PUT,
+      new s3n.LambdaDestination(handler)
+    );
   }
 }
